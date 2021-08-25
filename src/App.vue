60a5cbf3ee61fa27f39074f34c97843ad667d4e5
Mingyue Li <3249977074@qq.com>
2021-08-26 00:21:03 +0800

    feat: 增加tailwind daisyui

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- default theme (dark) -->
+  <div class="p-10 bg-base-100 text-base-content">
+    <button class="btn">button</button>
+    <button class="btn btn-primary">button</button>
+    <button class="btn btn-secondary">button</button>
+    <button class="btn btn-accent">button</button>
+  </div>
+
+  <!-- garden theme -->
+  <div class="p-10 bg-base-100 text-base-content" data-theme=garden>
+    <button class="btn">button</button>
+    <button class="btn btn-primary">button</button>
+    <button class="btn btn-secondary">button</button>
+    <button class="btn btn-accent">button</button>
+  </div>
+
+  <!-- forest theme -->
+  <div class="p-10 bg-base-100 text-base-content" data-theme=forest>
+    <button class="btn">button</button>
+    <button class="btn btn-primary">button</button>
+    <button class="btn btn-secondary">button</button>
+    <button class="btn btn-accent">button</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
 </script>
 
 <style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
